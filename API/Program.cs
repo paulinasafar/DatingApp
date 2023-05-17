77cdba386b1c0869708dbdb73449f42de0a35ff8
Paulina Safar <paulina.safar@yahoo.com>
2023-05-17 15:34:20 +0200

    Walking Skeleton C# & Angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,6 +30,9 @@ var app = builder.Build();
 
 //app.UseAuthorization();
 
+//*********Configure the HTTP request pipeline.************
+//****************Middleware*******************************
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
